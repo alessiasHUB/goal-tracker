3123b00bc2d3ada2122e0a6ec669c55e36488e82
alessiaborys <borys.alessia@gmail.com>
2023-08-16 18:31:21 +0100

    added lint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,9 @@
 import Footer from "../components/Footer";
 import "../styles/global.css";
-import { useState } from "react";
 import Header from "../components/Header";
 import { AppProps } from "next/app";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const [downloads, setDownloads] = useState<string>("");
-  const [input, setInput] = useState<string>("");
-
-  const getDownloads = async () => {
-    const res = await fetch("http://localhost:3000/api/getDownloads", {
-      method: "POST",
-      body: JSON.stringify({ input }),
-    });
-    const { downloads } = await res.json();
-    setDownloads(downloads);
-  };
-
   return (
     <div className="background">
       <Header />
